663c8207ff47f8da4914d836329f72faa604aad8
Alexis Tessier <alexistessierdev@gmail.com>
2017-10-16 20:38:12 +0200

    use string-argv to test the bin

--- tests/specs/bin.test.js
+++ tests/specs/bin.test.js
@@ -5,13 +5,15 @@ const test = require('ava');
 const pathFromIndex = require('../utils/path-from-index');
 const requireFromIndex = require('../utils/require-from-index');
 
+const argv = require('string-argv');
+
 test('Basic linking', t => {
 	const pkg = requireFromIndex('package.json');
 
 	t.is(typeof pkg.bin, 'object');
 	t.is(pkg.bin.nosg, 'bin/nosg');
 
-	process.argv = ['one', 'two', '#'];
+	process.argv = argv('node nosg.js #');
 
 	const detectCall = t.throws(() => {
 		requireFromIndex('bin/nosg');
